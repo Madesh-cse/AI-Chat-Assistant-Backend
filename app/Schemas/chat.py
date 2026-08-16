8f9e0fd2c842f15ffdf7c5001bdd26610771407b
Madesh M <madeshdex10@gmail.com>
2026-08-16 19:11:38 +0530

    update a chat schema

--- app/Schemas/chat.py
+++ app/Schemas/chat.py
@@ -4,6 +4,7 @@ from typing import Optional
 class ChatRequest(BaseModel):
     message: str
     conversation_id : Optional[int] = None
+    stack_overflow_enabled: bool = False
     
 class ChatResponse(BaseModel):
     response: str
