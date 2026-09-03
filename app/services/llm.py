73bfba4308b75c296e2649d0f551da133beb8049
Madesh M <madeshdex10@gmail.com>
2026-09-03 17:45:08 +0530

    update a service and database

--- app/services/llm.py
+++ app/services/llm.py
@@ -1,6 +1,8 @@
 import os
 
-from langchain_ollama import ChatOllama # type: ignore
+# from langchain_ollama import ChatOllama
+
+from langchain_groq import ChatGroq  # type: ignore
 from app.tools.weather import get_weather
 from app.tools.city_image import get_city_image
 from app.tools.news import get_news
@@ -15,15 +17,26 @@ from app.tools.notion import (
     read_notion_page,
 )
 
-llm = ChatOllama(
-    model = "qwen2.5:3b",
-    temperature=0.7,
-    base_url= os.getenv(
-        "OLLAMA_BASE_URL",
-        "http://localhost:11434"
-    ),
+# llm = ChatOllama(
+#     model = "qwen2.5:3b",
+#     temperature=0.7,
+#     base_url= os.getenv(
+#         "OLLAMA_BASE_URL",
+#         "http://localhost:11434"
+#     ),
     
+# )
+
+llm = ChatGroq(
+    api_key=os.getenv("GROQ_API_KEY"),
+    model=os.getenv(
+        "GROQ_MODEL",
+        "llama-3.3-70b-versatile",
+    ),
+    temperature=0.7,
 )
+
+
 llm_with_tools = llm.bind_tools(
     [get_weather,get_city_image, get_news,search_wikipedia,web_search,get_movie,   # Plugin
 search_stackoverflow, search_notion,read_notion_page ]
